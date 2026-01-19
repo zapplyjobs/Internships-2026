1319d0184a654d590abfbd47d49440c763965bf7
z-apply <admin@zapply.jobs>
2026-01-19 12:55:01 +0500

    more resource repositioning

--- .github/scripts/job-fetcher/readme-generator.js
+++ .github/scripts/job-fetcher/readme-generator.js
@@ -382,7 +382,17 @@ Check out what we're doing on our socials, join our community to connect with fe
 
 ---
 
+## Fresh Internships 2026
+
+<img src="images/insights.png" alt="Insights pulled from current listings.">
+
+${generateJobTable(currentJobs)}
+
+---
+
 ## More Resources
+
+Check out our other repos for jobs and free resources:
 
 <p align="center">
   <a href="https://github.com/zapplyjobs/New-Grad-Software-Engineering-Jobs-2026"><img src="images/repo-sej.png" alt="Software Engineering Jobs" height="40"></a>
@@ -405,14 +415,6 @@ Check out what we're doing on our socials, join our community to connect with fe
   &nbsp;&nbsp;
   <a href="https://github.com/zapplyjobs/underclassmen-internships"><img src="images/repo-uci.png" alt="Underclassmen Internships" height="40"></a>
 </p>
-
----
-
-## Fresh Internships 2026
-
-<img src="images/insights.png" alt="Insights pulled from current listings.">
-
-${generateJobTable(currentJobs)}
 
 ---
 
